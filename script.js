f912d6111fef64ffa4f9d34cca4c95037ce88345
extf8 <extfield8@gmail.com>
2022-05-21 16:44:03 +0300

    Added first row of buttons in html and style
    Added tooltip for proc and brackets buttons
    
    Added del butt func to scrip.js

--- script.js
+++ script.js
@@ -98,6 +98,7 @@ class Calculator {
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
